87af47d03c864cc918cc826537f097e9df0774e6
Micho Garcia <michogar@gmail.com>
2016-01-11 10:59:59 +0100

    Modified path to save pass.json file.
    Refactor path is needed :exclamation:

--- bin/create.js
+++ bin/create.js
@@ -5,23 +5,55 @@ var fs = require('fs'),
     jsonfile = require('jsonfile');
 
 var pass = {
-    "developer": "here your developer pass",
-    "publish": "here your publisher pass"
+    "developer": {
+        "app_id": "here your developer app ID",
+        "app_key": "here your developer app key"
+    },
+    "publish": {
+        "app_id": "here your publisher app ID",
+        "app_key": "here your publisher app key"
+    }
+};
+
+var schema = {
+    "properties": {
+        "developer_app_id": {
+            description: 'Enter your developer app_id',
+            required: true
+        },
+        'developer_app_key': {
+            description: 'Enter your developer app_key',
+            required: true
+        },
+        'publish_app_id': {
+            description: 'Enter your publisher app_id (empty to use developer)'
+        },
+        'publish_app_key': {
+            description: 'Enter your publisher app_key (empty to use developer)'
+        }
+    }
 };
 
 prompt.start();
 
 console.log('Insert your TMB API passwords:');
 
-prompt.get(['developer', 'publish'], function (err, result) {
-    console.log('Passwords you have writed:');
-    console.log('  developer: ' + result.developer);
-    console.log('  publish: ' + result.publish);
+prompt.get(schema, function (err, result) {
 
-    pass.developer = result.developer;
-    pass.publish = result.publish;
+    pass.developer.app_id = result.developer_app_id;
+    pass.developer.app_key = result.developer_app_key;
+    if (result.publish_app_id) {
+        pass.publish.app_id = result.publish_app_id;
+    } else {
+        pass.publish.app_id = result.developer_app_id
+    }
+    if (result.publish_app_key) {
+        pass.publish.app_key = result.publish_app_key;
+    } else {
+        pass.publish.app_key = result.developer_app_key
+    }
 
-    jsonfile.writeFile('../pass.json', pass, function (err) {
+    jsonfile.writeFile('./pass.json', pass, function (err) {
         if (err) {
             console.error(err);
         }

a650a252ddec60e1ee2c2f45f383fd75ca200e6a
Micho Garcia <michogar@gmail.com>
2016-01-11 10:19:07 +0100

    Added jsonfile module Now module creates pass.json in root module folder

--- bin/create.js
+++ bin/create.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    prompt = require('prompt');
+    prompt = require('prompt'),
+    jsonfile = require('jsonfile');
 
 var pass = {
     "developer": "here your developer pass",
     "publish": "here your publisher pass"
 };
+
+prompt.start();
+
+prompt.get(['developer', 'publish'], function (err, result) {
+    console.log('Command-line input received:');
+    console.log('  developer: ' + result.developer);
+    console.log('  publish: ' + result.publish);
+
+    pass.developer = result.developer;
+    pass.publish = result.publish;
+
+    jsonfile.writeFile('../pass.json', pass, function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+
 

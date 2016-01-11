7fb0ac9774fa57cb0ce950a1c69a7b2d8608590b
Micho Garcia <michogar@gmail.com>
2016-01-11 10:03:02 +0100

    Added .gitignore package.json with prompt dependencie

---
+++ bin/create.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    prompt = require('prompt');
+
+var pass = {
+    "developer": "here your developer pass",
+    "publish": "here your publisher pass"
+};
+

c80211f68b4808a02c6793d9d3a6145dfe3a8949
Micho Garcia <michogar@gmail.com>
2016-01-12 09:09:04 +0100

    Modified strucutre to use module.exports again

--- bin/create.js
+++ bin/create.js
@@ -17,7 +17,7 @@ var pass = {
     }
 };
 
-var schema = {
+var schemaDefault = {
     "properties": {
         "developer_app_id": {
             description: 'Enter your developer app_id',
@@ -36,33 +36,39 @@ var schema = {
     }
 };
 
-var Create = function() {
+var Create = {
 
-    prompt.start();
+    create : function(options) {
 
-    console.log('Insert your TMB API passwords:');
+        prompt.start();
 
-    prompt.get(schema, function (err, result) {
+        console.log('Insert your TMB API passwords:');
 
-        pass.developer.app_id = result.developer_app_id;
-        pass.developer.app_key = result.developer_app_key;
-        if (result.publish_app_id) {
-            pass.publish.app_id = result.publish_app_id;
-        } else {
-            pass.publish.app_id = result.developer_app_id
-        }
-        if (result.publish_app_key) {
-            pass.publish.app_key = result.publish_app_key;
-        } else {
-            pass.publish.app_key = result.developer_app_key
-        }
+        var schema = (options && options.schema) ? options.schema : schemaDefault;
 
-        jsonfile.writeFile('./pass.json', pass, function (err) {
-            if (err) {
-                console.error(err);
+        prompt.get(schema, function (err, result) {
+
+            pass.developer.app_id = result.developer_app_id;
+            pass.developer.app_key = result.developer_app_key;
+            if (result.publish_app_id) {
+                pass.publish.app_id = result.publish_app_id;
+            } else {
+                pass.publish.app_id = result.developer_app_id
             }
+            if (result.publish_app_key) {
+                pass.publish.app_key = result.publish_app_key;
+            } else {
+                pass.publish.app_key = result.developer_app_key
+            }
+
+            jsonfile.writeFile('./pass.json', pass, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
         });
-    });
+    }
+
 };
 
 module.exports = Create;
---
+++ spec/createSpec.js
@@ -0,0 +1,7 @@
+var create = require("../bin/create.js");
+
+describe("Testing if JSON pass file is created", function() {
+    describe("JSON file is created", function() {
+        create.create();
+    })
+});
